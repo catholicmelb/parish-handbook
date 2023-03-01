cbf5dc6577c51d91d46553b203a478585b6ca083
Leighton Hughes <hello@leightonhughes.co.nz>
2023-03-01 16:35:19 +1100

    Update

--- docs/public/admin/cms.js
+++ docs/public/admin/cms.js
@@ -1,22 +1,49 @@
-import CMS from 'netlify-cms-app';
-import posts from '@/cms/collections/posts';
+import { useEffect } from "react";
+import CMS from "netlify-cms-app";
+import React from "react";
 
-CMS.init({
-  config: {
-    load_config_file: false,
-    backend: {
-      name: 'github',
-      branch: 'main',
-      repo: 'leightonhughes/Vitepress-with-Decap-CMS'
-    },
-    media_folder: '/docs/public/media',
-    public_folder: '/media',
-    collections: [
+const Preview = (props) => {
+  const entry = props.entry;
+  const title = entry.getIn(["data", "title"]) || "";
+
+  return (
+    <>
+      <h1>{props.widgetFor("title")}</h1>
+      {props.widgetFor("body")}
+    </>
+  );
+};
+
+const Admin = () => {
+  useEffect(() => {
+    CMS.init(
       {
-        name: "post",
-        label: "Post",
-        folder: "docs/post"
+        config: {
+          load_config_file: false,
+          backend: {
+            name: 'github',
+            branch: 'main',
+            repo: 'leightonhughes/Vitepress-with-Decap-CMS'
+          },
+          media_folder: '/docs/public/media',
+          public_folder: '/media',
+          collections: [
+            {
+              name: "post",
+              label: "Post",
+              folder: "docs/post"
+            }
+          ]
+        }
       }
-    ]
-  }
-});
+    );
+    CMS.registerPreviewTemplate("blog", Preview);
+  }, []);
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default Admin;
