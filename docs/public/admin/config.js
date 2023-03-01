4173084c0db18bfb24282062a5093fac67e3570d
Leighton Hughes <hello@leightonhughes.co.nz>
2023-03-01 17:56:43 +1100

    Update

--- docs/public/admin/config.js
+++ docs/public/admin/config.js
@@ -1,3 +1,12 @@
 import CMS from 'netlify-cms-app';
 
-CMS.init();
+CMS.init({
+    config: {
+        load_config_file: false,
+        backend: {
+          name: 'git',
+          branch: 'main',
+          repo: 'leightonhughes/Vitepress-with-Decap-CMS'
+        },
+    }
+});
