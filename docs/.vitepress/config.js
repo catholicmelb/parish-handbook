3113a6c20319b06cd22523cc08e771c85422f754
Leighton Hughes <hello@leightonhughes.co.nz>
2023-03-01 17:30:00 +1100

    Revert

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,5 +10,13 @@ export default defineConfig({
     editLinks: true,
     editLinkText: 'Edit this page on GitHub',
     lastUpdated: 'Last Updated',
+    sidebar: {
+      { 
+        items: [
+          { text: 'Foreword', link: '/foreword.md' },
+          { text: 'Test', link: '/test.md' },
+        ]
+      }
+    }
   }
 })
