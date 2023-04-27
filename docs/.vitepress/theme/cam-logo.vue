bf2b575b98f760af7e68fea72b8d2fd8517bf878
Leighton Hughes <hello@leightonhughes.co.nz>
2023-04-27 10:25:18 +1000

    Logo added

---
+++ docs/.vitepress/theme/cam-logo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="handbook-footer">
+    <span>Created by:</span>
+    <a
+      href="https://melbournecatholic.org"
+      target="_blank"
+      rel="noopener"
+    >
+      <img
+        src="/img/cam-logo.svg"
+        aria-label="Melbourne Catholic"
+      />
+    </a>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.handbook-footer {
+  padding: 0 1.5rem 2rem;
+  font-size: 0.8rem;
+}
+
+.handbook-footer img {
+  box-sizing: border-box;
+  max-width: 200px;
+  height: 80px;
+  display: block;
+  margin: 1rem 0;
+  padding-left: 0.7rem;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,44 @@
 // .vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
+import Theme from 'vitepress/theme'
+import {h, watch} from 'vue'
 import './fonts.css'
 import './custom.css'
 
-export default DefaultTheme
+import NYSLogo from './cam-logo.vue';
+
+// Could also come from .env
+const GA_ID = 'UA-69117511-1';
+
+export default {
+  ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+        'aside-bottom': () => h(NYSLogo)
+      }
+    )
+  },
+//   enhanceApp: (ctx) => {
+//     // Google analytics integration
+//     if (import.meta.env.PROD && GA_ID && typeof window !== 'undefined') {
+//       (function (i, s, o, g, r, a, m) {
+//         i['GoogleAnalyticsObject'] = r
+//         i[r] = i[r] || function () {
+//           (i[r].q = i[r].q || []).push(arguments)
+//         }
+//         i[r].l = 1 * new Date()
+//         a = s.createElement(o)
+//         m = s.getElementsByTagName(o)[0]
+//         a.async = 1
+//         a.src = g
+//         m.parentNode.insertBefore(a, m)
+//       })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+//       ga('create', GA_ID, 'auto')
+//       ga('set', 'anonymizeIp', true)
+//       // Send a page view any time the route changes
+//       watch(ctx.router.route, (newValue, oldValue) => {
+//         ga('set', 'page', newValue.path)
+//         ga('send', 'pageview')
+//       })
+//     }
+//   }
+}
